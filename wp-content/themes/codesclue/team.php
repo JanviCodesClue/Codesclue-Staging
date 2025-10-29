7f5e28c5d67052ed46c0d4864a14adf301978519
Janvi <janvi.codesclue@gmail.com>
2025-10-29 11:16:46 +0530

    team page feedbake changes done

--- wp-content/themes/codesclue/team.php
+++ wp-content/themes/codesclue/team.php
@@ -14,7 +14,7 @@ get_header(); ?>
                     <div class="section-content-wrapper">
                         <div class="content-shape-1 fade-anim" data-direction="top" data-ease="bounce.out"
                             data-delay="0.60" data-offset="100" data-rotate="360">
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/new/shape-36.webp" alt="image">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/new/shape-36.webp" alt="image" class="w-100">
                         </div>
 
                         <div class="section-content">
@@ -46,19 +46,19 @@ get_header(); ?>
                 <div class="col-lg-6">
                     <div class="our-team">
                         <h2 class="our-team-h2">Our Team</h2>
-                        <img class="p-8" src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/team-frist.png" alt="image">
-                        <p class="team-dec p-8">100% Confidential and Secure</p>
-                        <h4 class="taem-inner-heading p-8">Our team comprises experts, innovators, and achievers!</h4>
-                        <p class="team-dec p-8">CodesClue is not just any ordinary mobile app and software development company but also a family of forward looking top industry talents. Whether it’s enterprises, SMEs, or even startups, our dedicated teams are ready to shape your ideas into innovative solutions.</p>
+                        <img class="pt-16 w-100" src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/team-frist.png" alt="image">
+                        <p class="team-dec pt-16">100% Confidential and Secure</p>
+                        <h4 class="taem-inner-heading pt-16">Our team comprises experts, innovators, and achievers!</h4>
+                        <p class="team-dec pt-16">CodesClue is not just any ordinary mobile app and software development company but also a family of forward looking top industry talents. Whether it’s enterprises, SMEs, or even startups, our dedicated teams are ready to shape your ideas into innovative solutions.</p>
                     </div>
                 </div>
                 <div class="col-lg-6">
                 <p class="team-dec">At CodesClue, our team is at the heart of our success. We are a group of highly skilled professionals, united by a shared vision to deliver innovative, scalable, and impactful digital solutions.</p>
-                <p class="team-dec p-8">With expertise spanning web and mobile application development, AI powered systems, and industry specific platforms, our team brings together technical excellence and strategic thinking to help businesses thrive in an ever evolving digital landscape</p>
-                <div class="p-8">
+                <p class="team-dec pt-16">With expertise spanning web and mobile application development, AI powered systems, and industry specific platforms, our team brings together technical excellence and strategic thinking to help businesses thrive in an ever evolving digital landscape.</p>
+                <div class="pt-16">
                     <a href="<?php echo home_url('/contact-us/'); ?>" class="btn btn-custom">LET'S CONNECT →</a>
                 </div>
-                <img class="p-8" src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/team-second.png" alt="image">
+                <img class="pt-16" src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/team-second.png" alt="image" class="w-100">
                 </div>
             </div>
   </div>
@@ -67,19 +67,19 @@ get_header(); ?>
                 <div class="col-lg-6">
                     <div class="our-team">
                         <h2 class="our-team-h2">Our Team</h2>
-                        <img class="p-8" src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/team-frist.png" alt="image">
-                           <p class="team-dec">At CodesClue, our team is at the heart of our success. We are a group of highly skilled professionals, united by a shared vision to deliver innovative, scalable, and impactful digital solutions.</p>
-                <p class="team-dec p-8">With expertise spanning web and mobile application development, AI powered systems, and industry specific platforms, our team brings together technical excellence and strategic thinking to help businesses thrive in an ever evolving digital landscape</p>
-                <div class="p-8">
+                        <img class="pt-16-m w-100" src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/team-frist.png" alt="image">
+                           <p class="team-dec pt-16-m">At CodesClue, our team is at the heart of our success. We are a group of highly skilled professionals, united by a shared vision to deliver innovative, scalable, and impactful digital solutions.</p>
+                <p class="team-dec pt-16-m">With expertise spanning web and mobile application development, AI powered systems, and industry specific platforms, our team brings together technical excellence and strategic thinking to help businesses thrive in an ever evolving digital landscape.</p>
+                <div class="pt-16-m">
                     <a href="<?php echo home_url('/contact-us/'); ?>" class="btn btn-custom">LET'S CONNECT →</a>
                 </div>
                     </div>
                 </div>
                 <div class="col-lg-6">
-                <img class="p-8" src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/team-second.png" alt="image">
-                <p class="team-dec p-8">100% Confidential and Secure</p>
-                        <h4 class="taem-inner-heading p-8">Our team comprises experts, innovators, and achievers!</h4>
-                        <p class="team-dec p-8">CodesClue is not just any ordinary mobile app and software development company but also a family of forward looking top industry talents. Whether it’s enterprises, SMEs, or even startups, our dedicated teams are ready to shape your ideas into innovative solutions.</p>
+                <img class="pt-16-m w-100" src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/team-second.png" alt="image">
+                <p class="team-dec pt-16-m">100% Confidential and Secure</p>
+                        <h4 class="taem-inner-heading pt-16-m">Our team comprises experts, innovators, and achievers!</h4>
+                        <p class="team-dec pt-16-m">CodesClue is not just any ordinary mobile app and software development company but also a family of forward looking top industry talents. Whether it’s enterprises, SMEs, or even startups, our dedicated teams are ready to shape your ideas into innovative solutions.</p>
                 </div>
             </div>
   </div>
@@ -91,12 +91,12 @@ get_header(); ?>
             <div class="co-card">
                 <div class="row">
                     <div class="col-lg-5">
-                        <img class="p-8" src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/co-img.png" alt="image">
+                        <img class="w-100" src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/co-img.png" alt="image">
                     </div>
-                    <div class="col-lg-7 align-self-center">
+                    <div class="col-lg-7 align-self-center pt-16-m">
                         <p class="team-dec">Ketan Patel is the driving force behind the remarkable success of CodesClue. With over 14 years of experience in leadership and management within software and product development, he has been instrumental in shaping the company’s journey.</p>
                         <p class="team-dec pt-16">His expertise spans across diverse areas of the IT industry, including consulting, design, project management, business development, innovation, and strategic growth. Above all, his visionary leadership has been key to CodesClue’s achievements and continued evolution.</p>
-                        <p class="team-dec pt-16">He is obsessed with delivering quality to the organization and to all CodesClue partners and clientele</p>
+                        <p class="team-dec pt-16">He is obsessed with delivering quality to the organization and to all CodesClue partners and clientele.</p>
                         <h5 class="co-name pt-16">Ketan Patel</h5>
                         <p class="team-dec ">Co-Founder</p>
                     </div>
@@ -105,7 +105,7 @@ get_header(); ?>
     </div>
   </div>
 
-  <section class="life-section pt-40">
+  <section class="life-section pt-40 d-hide">
     <div class="container">
       <div class="row g-3 align-items-center">
          <div class="col-2">
@@ -117,36 +117,75 @@ get_header(); ?>
         <div class="col-2">
           <img src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/codesclue (103).png" class="" alt="">
         </div>
-        <div class="col-2 m-hide">
+        <div class="col-2">
           <img src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/codesclue (104).png" class="" alt="">
         </div>
-        <div class="col-2 m-hide">
+        <div class="col-2">
           <img src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/codesclue (105).png" class="" alt="">
         </div>
-        <div class="col-2 m-hide">
+        <div class="col-2">
           <img src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/codesclue (106).png" class="" alt="">
         </div>
-        <div class="col-2  ">
+        <div class="col-2">
           <img src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/codesclue (107).png" class="" alt="">
         </div>
-        <div class="col-2  d-flex flex-column gap-3">
+        <div class="col-2 d-flex flex-column gap-3">
           <img src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/codesclue (108).png" class="" alt="">
         </div>
-        <div class="col-4   m-hide">
+        <div class="col-4">
           <h2>Life @ <span>CodesClue</span></h2>
           <p>Our vibrant culture empowers creativity, collaboration, and continuous growth.</p>
         </div>
-           <div class="col-2  d-flex flex-column gap-3">
+           <div class="col-2 d-flex flex-column gap-3">
           <img src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/codesclue (109).png" class="" alt="">
         </div>
-        <div class="col-2  m-hide ">
+        <div class="col-2">
           <img src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/codesclue (110).png" class="" alt="">
         </div>
       </div>
     </div>
   </section>
 
-
+  <section class="life-section pt-40 m-hide">
+    <div class="container">
+      <div class="row g-3 align-items-center justify-content-center">
+        <div class="col-12">
+          <h2>Life @ <span>CodesClue</span></h2>
+          <p>Our vibrant culture empowers creativity, collaboration, and continuous growth.</p>
+        </div>
+        <div class="col-3-m">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/codesclue (101).png" class="" alt="">
+        </div>
+        <div class="col-3-m">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/codesclue (102).png" class="" alt="">
+        </div>
+        <div class="col-3-m">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/codesclue (103).png" class="" alt="">
+        </div>
+        <div class="col-3-m">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/codesclue (104).png" class="" alt="">
+        </div>
+        <div class="col-3-m">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/codesclue (105).png" class="" alt="">
+        </div>
+        <div class="col-3-m">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/codesclue (106).png" class="" alt="">
+        </div>
+        <div class="col-3-m">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/codesclue (107).png" class="" alt="">
+        </div>
+        <div class="col-3-m">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/codesclue (108).png" class="" alt="">
+        </div>
+        <div class="col-3-m">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/codesclue (109).png" class="" alt="">
+        </div>
+        <div class="col-3-m">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/codesclue (110).png" class="" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
 
 
 
@@ -161,26 +200,28 @@ get_header(); ?>
   <div class="container">
     <div class="row  pt-40">
         <div class="col-lg-4 col-md-12 col-sm-12">
-           <img class="p-8" src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/innovation.png" alt="image">
-           <h4 class="taem-inner-heading">Innovation</h4>
+           <img class="w-100" src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/innovation.png" alt="image">
+           <h4 class="taem-inner-heading pt-16-m">Innovation</h4>
            <p class="team-dec pt-16">At CodesClue, innovation is at the core of everything we do. We don’t just build software; we create intelligent, future ready solutions that empower businesses to stay ahead in a rapidly evolving digital world.</p>
            <p class="team-dec pt-16">By combining cutting edge technologies, user centric design, and a deep understanding of industry challenges, we deliver products and platforms that drive growth and transformation.</p>
         </div>
          <div class="col-lg-4 col-md-12 col-sm-12 d-only-lg-sec">
             <h4 class="taem-inner-heading">Collaboration</h4>
            <p class="team-dec pt-16">At CodesClue, we believe collaboration is the foundation of every successful project. We work closely with our clients as partners, aligning our expertise with their vision to deliver meaningful results.</p>
-           <p class="team-dec pt-16">By fostering open communication, transparency, and teamwork, we ensure every stakeholder is involved at every stage of the process.<stong>Together, we innovate, build, and grow</strong></p>
-           <img class="p-8" src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/collaboration.png" alt="image">
+           <p class="team-dec pt-16">By fostering open communication, transparency, and teamwork, we ensure every stakeholder is involved at every stage of the process.</p>
+           <p class="team-dec"><strong>Together, we innovate, build, and grow</strong></p>
+           <img class="pt-16 w-100" src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/collaboration.png" alt="image">
         </div>
          <div class="col-lg-4 col-md-12 col-sm-12 d-only-md-sec">
-           <img class="p-8" src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/collaboration.png" alt="image">
-           <h4 class="taem-inner-heading">Collaboration</h4>
+           <img class="pt-16 w-100" src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/collaboration.png" alt="image">
+           <h4 class="taem-inner-heading pt-16-m">Collaboration</h4>
            <p class="team-dec pt-16">At CodesClue, we believe collaboration is the foundation of every successful project. We work closely with our clients as partners, aligning our expertise with their vision to deliver meaningful results.</p>
-           <p class="team-dec pt-16">By fostering open communication, transparency, and teamwork, we ensure every stakeholder is involved at every stage of the process.<stong>Together, we innovate, build, and grow</strong></p>
+           <p class="team-dec pt-16">By fostering open communication, transparency, and teamwork, we ensure every stakeholder is involved at every stage of the process.</p>
+           <p class="team-dec pb-16-m"><strong>Together, we innovate, build, and grow</strong></p>
         </div>
          <div class="col-lg-4 col-md-12 col-sm-12">
-           <img class="p-8" src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/excellence.png" alt="image">
-           <h4 class="taem-inner-heading">Excellence</h4>
+           <img class="pt-16 w-100" src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/excellence.png" alt="image">
+           <h4 class="taem-inner-heading pt-16-m">Excellence</h4>
            <p class="team-dec pt-16">At CodesClue, excellence is not just a standard it’s our commitment. We strive for perfection in every line of code, every design detail, and every client interaction.</p>
            <p class="team-dec pt-16">Our team is dedicated to delivering high quality, reliable, and scalable solutions that stand the test of time.</p>
            <p class="team-dec pt-16"><strong>Excellence is at the heart of everything we do</strong></p>
@@ -189,14 +230,21 @@ get_header(); ?>
     </div>
   </div>
 
+  <div class="container">
+    <div class="pt-40">
+       <div class="divaider">
+    </div>
+    </div>
+  </div>
+
 
   <div class="container">
     <div class="row pt-40 pb-40">
         <div class="col-lg-3 col-md-3 col-sm-12">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/last-team.png" alt="image">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/teams/last-team.png" alt="image" class="w-100">
         </div>
         <div class="col-lg-7 col-md-7 col-sm-12">
-            <h4 class="taem-inner-heading">5+ years of relationship building experience</h4>
+            <h4 class="taem-inner-heading pt-16-m">5+ years of relationship building experience</h4>
            <p class="team-dec pt-16">We are committed to transparency and accountability in our business. We strive to achieve collaborative relationships that are trustworthy and have mutual respect.</p>
            <div class="pt-16">
                     <a href="<?php echo home_url('/contact-us/'); ?>" class="btn btn-custom">LET'S CONNECT →</a>
